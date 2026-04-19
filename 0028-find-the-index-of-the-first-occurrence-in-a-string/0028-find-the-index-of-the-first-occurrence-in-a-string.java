d522adf60b972d3832e75f874b60444224eb00fe
vtu25672-max <vtu25672@veltech.edu.in>
2026-04-19 21:37:33 +0530

    Create 0028-find-the-index-of-the-first-occurrence-in-a-string.java

---
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        
+        if (needle.length() == 0) {
+            return 0;
+        }
+        
+        int n = haystack.length();
+        int m = needle.length();
+        
+        for (int i = 0; i <= n - m; i++) {
+            
+            int j = 0;
+            
+            while (j < m && haystack.charAt(i + j) == needle.charAt(j)) {
+                j++;
+            }
+            
+            if (j == m) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+}
